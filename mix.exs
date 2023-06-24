d0a220bb200368fd5e96fd3fc2b55a72d638e30c
Joey Chilson <100753878+joeychilson@users.noreply.github.com>
2023-06-24 03:56:50 -0500

    Added license and updated project info

--- mix.exs
+++ mix.exs
@@ -1,29 +1,40 @@
 defmodule FMP.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @description "An Elixir-based HTTP Client for financialmodelingprep.com"
+
   def project do
     [
       app: :fmp_client,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: @description,
+      deps: deps(),
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger, :httpoison, :jason]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:httpoison, "~> 2.1"},
       {:jason, "~> 1.4"},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/joeychilson/fmp_client"},
+      maintainers: ["Joey Chilson"]
     ]
   end
 end
