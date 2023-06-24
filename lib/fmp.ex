d1e7d42ec4a36fa73c1bc289150b4f46ecaa721e
Joey Chilson <100753878+joeychilson@users.noreply.github.com>
2023-06-24 02:41:52 -0500

    Initial code commit

--- lib/fmp.ex
+++ lib/fmp.ex
@@ -1,6 +1,43 @@
 defmodule FMP do
+  alias FMP.Profile
 
-  def hello do
-    :world
+  @base_url "https://financialmodelingprep.com/api/v3"
+
+  @doc """
+  Fetches a company profile from the FMP API.
+
+  ## Examples
+
+    iex> {:ok, profile} = FMP.get_profile("AAPL")
+    iex> profile.symbol
+    "AAPL"
+  """
+  def get_profile(symbol) do
+    resp = get("#{@base_url}/profile/#{symbol}")
+
+    case resp do
+      {:ok, []} ->
+        {:error, :not_found}
+      {:ok, resp} ->
+        {:ok, Profile.from_json(resp)}
+      _ ->
+        resp
+    end
+  end
+
+  @doc false
+  def get(url) do
+    api_key = Application.get_env(:fmp_client, :api_key)
+
+    case HTTPoison.get("#{url}?apikey=#{api_key}") do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        {:ok, Jason.decode!(body)}
+
+      {:ok, %HTTPoison.Response{status_code: code}} ->
+        {:error, {:unexpected_status_code, code}}
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, reason}
+    end
   end
 end
--- mix.exs
+++ mix.exs
@@ -14,15 +14,16 @@ defmodule FMP.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :httpoison, :jason]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:httpoison, "~> 2.1"},
+      {:jason, "~> 1.4"},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
   end
 end
