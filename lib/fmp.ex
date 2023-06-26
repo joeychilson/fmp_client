19dc8c760243459b55f754d705d611219bfc2ce4
Joey Chilson <100753878+joeychilson@users.noreply.github.com>
2023-06-25 20:19:08 -0500

    Removed structs, and simplifed get func

--- lib/fmp.ex
+++ lib/fmp.ex
@@ -1,26 +1,4 @@
 defmodule FMP do
-  alias FMP.EnterpriseValue
-  alias FMP.FinancialRatios
-
-  alias FMP.{
-    IncomeStatement,
-    BalanceSheet,
-    CashFlowStatement,
-    FinancialRatios,
-    FinancialScores,
-    EnterpriseValue,
-    ETF,
-    ETFHolding,
-    ETFExposure,
-    ETFCountryWeight,
-    ETFSectorWeight,
-    KeyExecutive,
-    MarketCap,
-    Peers,
-    Profile,
-    Symbol
-  }
-
   @api_v3 "https://financialmodelingprep.com/api/v3"
   @api_v4 "https://financialmodelingprep.com/api/v4"
 
@@ -38,7 +16,7 @@ defmodule FMP do
     true
   """
   def get_income_statements(cik_or_symbol, params \\ %{}),
-    do: get_data(IncomeStatement, "#{@api_v3}/income-statement/#{cik_or_symbol}", params)
+    do: get("#{@api_v3}/income-statement/#{cik_or_symbol}", params)
 
   @doc """
   Fetches a company's balance sheets from the FMP API.
@@ -54,7 +32,7 @@ defmodule FMP do
     true
   """
   def get_balance_sheets(cik_or_symbol, params \\ %{}),
-    do: get_data(BalanceSheet, "#{@api_v3}/balance-sheet-statement/#{cik_or_symbol}", params)
+    do: get("#{@api_v3}/balance-sheet-statement/#{cik_or_symbol}", params)
 
   @doc """
   Fetches a company's cash flow statements from the FMP API.
@@ -70,12 +48,7 @@ defmodule FMP do
     true
   """
   def get_cash_flow_statements(cik_or_symbol, params \\ %{}),
-    do:
-      get_data(
-        CashFlowStatement,
-        "#{@api_v3}/cash-flow-statement/#{cik_or_symbol}",
-        params
-      )
+    do: get("#{@api_v3}/cash-flow-statement/#{cik_or_symbol}", params)
 
   @doc """
   Fetches a company's financial ratios from the FMP API.
@@ -90,8 +63,7 @@ defmodule FMP do
     iex> Enum.count(ratios) == 1
     true
   """
-  def get_financial_ratios(symbol, params \\ %{}),
-    do: get_data(FinancialRatios, "#{@api_v3}/ratios/#{symbol}", params)
+  def get_financial_ratios(symbol, params \\ %{}), do: get("#{@api_v3}/ratios/#{symbol}", params)
 
   @doc """
   Fetches a company's financial scores from the FMP API.
@@ -102,8 +74,7 @@ defmodule FMP do
     iex> scores.symbol
     "AAPL"
   """
-  def get_financial_scores(symbol),
-    do: get_data(FinancialScores, "#{@api_v4}/score?symbol=#{symbol}")
+  def get_financial_scores(symbol), do: get("#{@api_v4}/score?symbol=#{symbol}")
 
   @doc """
   Fetches a company's enterprise value from the FMP API.
@@ -115,7 +86,7 @@ defmodule FMP do
     "AAPL"
   """
   def get_enterprise_value(symbol, params \\ %{}),
-    do: get_data(EnterpriseValue, "#{@api_v3}/enterprise-values/#{symbol}", params)
+    do: get("#{@api_v3}/enterprise-values/#{symbol}", params)
 
   @doc """
   Fetches a company's key executives from the FMP API.
@@ -126,8 +97,7 @@ defmodule FMP do
     iex> Enum.count(executives) > 0
     true
   """
-  def get_key_executives(symbol),
-    do: get_data(KeyExecutive, "#{@api_v3}/key-executives/#{symbol}")
+  def get_key_executives(symbol), do: get("#{@api_v3}/key-executives/#{symbol}")
 
   @doc """
   Fetches a company's market capitalization from the FMP API.
@@ -138,8 +108,7 @@ defmodule FMP do
     iex> market_cap.symbol
     "AAPL"
   """
-  def get_market_cap(symbol),
-    do: get_data(MarketCap, "#{@api_v3}/market-capitalization/#{symbol}")
+  def get_market_cap(symbol), do: get("#{@api_v3}/market-capitalization/#{symbol}")
 
   @doc """
   Fetches a company's historical market capitalization from the FMP API.
@@ -155,7 +124,7 @@ defmodule FMP do
     true
   """
   def get_historical_market_cap(symbol, params \\ %{}),
-    do: get_data(MarketCap, "#{@api_v3}/historical-market-capitalization/#{symbol}", params)
+    do: get("#{@api_v3}/historical-market-capitalization/#{symbol}", params)
 
   @doc """
   Fetches a company's peers from the FMP API.
@@ -166,8 +135,7 @@ defmodule FMP do
     iex> peers.symbol
     "AAPL"
   """
-  def get_peers(symbol),
-    do: get_data(Peers, "#{@api_v4}/stock_peers?symbol=#{symbol}")
+  def get_peers(symbol), do: get("#{@api_v4}/stock_peers?symbol=#{symbol}")
 
   @doc """
   Fetches a company profile from the FMP API.
@@ -178,8 +146,7 @@ defmodule FMP do
     iex> profile.symbol
     "AAPL"
   """
-  def get_profile(symbol),
-    do: get_data(Profile, "#{@api_v3}/profile/#{symbol}")
+  def get_profile(symbol), do: get("#{@api_v3}/profile/#{symbol}")
 
   @doc """
   Fetches the symbols of all companies from the FMP API.
@@ -190,8 +157,7 @@ defmodule FMP do
     iex> Enum.count(symbols) > 0
     true
   """
-  def get_symbols(),
-    do: get_data(Symbol, "#{@api_v3}/stock/list")
+  def get_symbols(), do: get("#{@api_v3}/stock/list")
 
   @doc """
   Fetches the symbols of all tradable companies from the FMP API.
@@ -202,8 +168,7 @@ defmodule FMP do
     iex> Enum.count(symbols) > 0
     true
   """
-  def get_tradable_symbols(),
-    do: get_data(Symbol, "#{@api_v3}/available-traded/list")
+  def get_tradable_symbols(), do: get("#{@api_v3}/available-traded/list")
 
   @doc """
   Fetches the symbols of all ETFs from the FMP API.
@@ -214,8 +179,7 @@ defmodule FMP do
     iex> Enum.count(symbols) > 0
     true
   """
-  def get_etfs(),
-    do: get_data(Symbol, "#{@api_v3}/etf/list")
+  def get_etfs(), do: get("#{@api_v3}/etf/list")
 
   @doc """
   Fetches information about an ETF from the FMP API.
@@ -226,8 +190,7 @@ defmodule FMP do
     iex> etf.symbol
     "SPY"
   """
-  def get_etf(symbol),
-    do: get_data(ETF, "#{@api_v4}/etf-info?symbol=#{symbol}")
+  def get_etf(symbol), do: get("#{@api_v4}/etf-info?symbol=#{symbol}")
 
   @doc """
   Fetches the holdings of an ETF from the FMP API.
@@ -238,8 +201,7 @@ defmodule FMP do
     iex> Enum.count(holdings) > 0
     true
   """
-  def get_etf_holdings(symbol),
-    do: get_data(ETFHolding, "#{@api_v3}/etf-holder/#{symbol}")
+  def get_etf_holdings(symbol), do: get("#{@api_v3}/etf-holder/#{symbol}")
 
   @doc """
   Fetches the stock exposure of an ETF from the FMP API.
@@ -250,8 +212,7 @@ defmodule FMP do
     iex> Enum.count(stock_exposure) > 0
     true
   """
-  def get_etf_stock_exposure(symbol),
-    do: get_data(ETFExposure, "#{@api_v3}/etf-stock-exposure/#{symbol}")
+  def get_etf_stock_exposure(symbol), do: get("#{@api_v3}/etf-stock-exposure/#{symbol}")
 
   @doc """
   Fetches the country weightings of an ETF from the FMP API.
@@ -262,8 +223,7 @@ defmodule FMP do
     iex> Enum.count(country_weightings) > 0
     true
   """
-  def get_etf_country_weightings(symbol),
-    do: get_data(ETFCountryWeight, "#{@api_v3}/etf-country-weightings/#{symbol}")
+  def get_etf_country_weightings(symbol), do: get("#{@api_v3}/etf-country-weightings/#{symbol}")
 
   @doc """
   Fetches the sector weightings of an ETF from the FMP API.
@@ -274,13 +234,14 @@ defmodule FMP do
     iex> Enum.count(sector_weightings) > 0
     true
   """
-  def get_etf_sector_weightings(symbol),
-    do: get_data(ETFSectorWeight, "#{@api_v3}/etf-sector-weightings/#{symbol}")
+  def get_etf_sector_weightings(symbol), do: get("#{@api_v3}/etf-sector-weightings/#{symbol}")
 
   @doc false
-  defp get(url) do
+  defp get(url, params \\ %{}) do
     api_key = Application.get_env(:fmp_client, :api_key)
     if api_key == nil, do: {:error, :api_key_not_set}
+
+    url = if params == %{}, do: url, else: "#{url}?#{URI.encode_query(params)}"
 
     url =
       if String.contains?(url, "?"),
@@ -289,29 +250,16 @@ defmodule FMP do
 
     case HTTPoison.get(url) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        {:ok, Jason.decode!(body)}
+        case Jason.decode!(body, keys: :atoms) do
+          [] -> {:error, :not_found}
+          data -> {:ok, data}
+        end
 
       {:ok, %HTTPoison.Response{status_code: code}} ->
         {:error, {:unexpected_status_code, code}}
 
       {:error, %HTTPoison.Error{reason: reason}} ->
         {:error, reason}
-    end
-  end
-
-  @doc false
-  defp get_data(struct, url, params \\ %{}) do
-    url = if params == %{}, do: url, else: "#{url}?#{URI.encode_query(params)}"
-
-    case get(url) do
-      {:ok, []} ->
-        {:error, :not_found}
-
-      {:ok, resp} ->
-        {:ok, struct.from_resp(resp)}
-
-      error ->
-        error
     end
   end
 end
